5be492f9f303a29a69bac4c03b4d79905c095a1f
Doxygen CI <ci-arduino@invalid>
2024-03-11 14:43:50 +0000

    Deploy docs to GitHub Pages from commit f3e2f4dd4d
    Commit: f3e2f4dd4dd6acb6375103f235d115d633a7dd40
    GitHub Actions run: 8234697006

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -3,5 +3,6 @@ var searchData=
   ['getframe',['getFrame',['../class_adafruit___m_l_x90640.html#abd22c69d88fcb32a393e1b9b0093097d',1,'Adafruit_MLX90640']]],
   ['getmode',['getMode',['../class_adafruit___m_l_x90640.html#ab15d600ef77fc83ffd9c66ea97014316',1,'Adafruit_MLX90640']]],
   ['getrefreshrate',['getRefreshRate',['../class_adafruit___m_l_x90640.html#a3bd41e8d87e2397eed3b541dc4b8fd28',1,'Adafruit_MLX90640']]],
-  ['getresolution',['getResolution',['../class_adafruit___m_l_x90640.html#a786b82e44deb5caab878094fb9cbe57a',1,'Adafruit_MLX90640']]]
+  ['getresolution',['getResolution',['../class_adafruit___m_l_x90640.html#a786b82e44deb5caab878094fb9cbe57a',1,'Adafruit_MLX90640']]],
+  ['getta',['getTa',['../class_adafruit___m_l_x90640.html#a2da6ff539540846b6dfbe5c8583abfe5',1,'Adafruit_MLX90640']]]
 ];
