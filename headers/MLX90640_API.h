fa8d5d9331311ae4354caab1e8a72ce3e6fa17dc
Anne Barela <1911920+TheKitty@users.noreply.github.com>
2025-10-21 15:38:18 -0500

    Fix formatting and re-add header comments
    ci wants space between // and #define

--- headers/MLX90640_API.h
+++ headers/MLX90640_API.h
@@ -21,7 +21,7 @@
 
 #define SCALEALPHA 0.000001
 
-//#define MLX90640_DEBUG
+// #define MLX90640_DEBUG
 
 typedef struct {
   int16_t kVdd;
@@ -54,3 +54,4 @@ typedef struct {
 } paramsMLX90640;
 
 #endif
+
